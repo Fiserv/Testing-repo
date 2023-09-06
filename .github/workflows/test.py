639eac97fd877eed30fdefa03b5d5b5b83280bf2
TaniaS <74629095+TaniaS-fiserv@users.noreply.github.com>
2023-09-05 22:46:09 -0400

    Update test.py

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -1,6 +1,11 @@
 import requests
 import json
 
+# Replace these with your GitHub repository information and personal access token
+#owner = "Fiserv"
+#repo = "Testing-repo"
+#token = "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"
+
 # Function to get a list of hook IDs for the repository
 def get_hook_ids():
     url = f"https://api.github.com/repos/Fiserv/Testing-repo/hooks"
@@ -16,6 +21,37 @@ def get_hook_ids():
     hook_ids = [hook["id"] for hook in hooks]
     return hook_ids
 
+# Function to redeliver failed deliveries with status code 500
+def redeliver_failed_deliveries(hook_id):
+    url = f"https://api.github.com/repos/Fiserv/Testing-repo/hooks/{hook_id}/deliveries"
+    headers = {
+        "Authorization": f"Bearer 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",
+        "Accept": "application/vnd.github.v3+json",
+        "X-GitHub-Api-Version": "2022-11-28"
+    }
+
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    deliveries = response.json()
+    
+    for delivery in deliveries:
+        if delivery["response"]["status"] == 200:
+            delivery_id = delivery["id"]
+            deliveries_url = f"https://api.github.com/repos/Fiserv/Testing-repo/hooks/{hook_id}/deliveries"
+            response = requests.post(deliveries_url, headers=headers)
+            response.raise_for_status()
+            print(f"Redelivered delivery ID {delivery_id} for hook ID {hook_id}")
+
+    # for delivery in deliveries:
+    #     if delivery["response"]["status"] == 500:
+    #         delivery_id = delivery["id"]
+    #         redeliver_url = f"https://api.github.com/repos/{owner}/{repo}/hooks/{hook_id}/deliveries/{delivery_id}/attempts"
+    #         response = requests.post(redeliver_url, headers=headers)
+    #         response.raise_for_status()
+    #         print(f"Redelivered delivery ID {delivery_id} for hook ID {hook_id}")
+
 if __name__ == "__main__":
     hook_ids = get_hook_ids()
     print("Hook IDs:", hook_ids)
+    for hook_id in hook_ids:
+        redeliver_failed_deliveries(hook_id)
